a50a741592e67451f2a3ce9e9456003087d7909f
Bodhidharma Ong <bodhidharma202@gmail.com>
2024-09-06 16:47:33 +0800

    modified projects section

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -7,25 +7,11 @@ function Projects() {
             <h1 className='project-title mb-5'>Projects</h1>
             <div className='card-div d-flex justify-content-evenly'>
                 <div class="card-projects" style={{width: '18rem'}}>
-                <div class="card-body">
-                    <h5 class="card-title">Quote Generator</h5>
-                    <p class="card-text">A simple quote generator that fetches and displays quotes based on various categories.</p>
-                    <a href="https://github.com/BodhiOng/Quote-Generator" class="card-link">Github Repository</a>
-                </div>
-                </div>
-                <div class="card-projects" style={{width: '18rem'}}>
-                <div class="card-body">
-                    <h5 class="card-title">Calculator</h5>
-                    <p class="card-text">A simple and responsive calculator that performs basic arithmetic operations.</p>
-                    <a href="https://github.com/BodhiOng/Calculator" class="card-link">Github Repository</a>
-                </div>
-                </div>
-                <div class="card-projects" style={{width: '18rem'}}>
-                <div class="card-body">
-                    <h5 class="card-title">Rent And Utilities Calculator</h5>
-                    <p class="card-text">A rent and utilities calculator app to manage tenants, track monthly expenses, and generate receipts for singular apartment unit.</p>
-                    <a href="https://github.com/BodhiOng/Rent-And-Utilities-Calculator" class="card-link">Github Repository</a>
-                </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Social Media Platform API (Express.js)</h5>
+                        <p class="card-text">RESTful API with JWT Auth & Rate limiting to replicate the average social media</p>
+                        <a href="https://github.com/BodhiOng/Social-Media-Platform-ExpressJS" class="card-link">Github Repository</a>
+                    </div>
                 </div>
             </div>
         </div>
