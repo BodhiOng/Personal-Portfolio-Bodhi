8b548d841fe163d14920e7b030ab1d6f3735e79a
Bodhi Ong <bodhidharma202@gmail.com>
2024-10-29 19:38:28 +0800

    build problem should be resolved by now

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  output: 'export',
+  distDir: 'build'
 };
 
 export default nextConfig;
